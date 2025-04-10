8c3e601f612a52d1165654593a5ce91e13ef38a4
Miguel Alejandro Gutierrez Ortega <72420178+Alejandrom-GO@users.noreply.github.com>
2025-04-10 17:29:10 -0600

    fix: actualizar configuración de Vite y Vercel para SPA

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [vue()],
-
+  base: '/',
+  
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -34,4 +35,9 @@ export default defineConfig(async () => ({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 }));
